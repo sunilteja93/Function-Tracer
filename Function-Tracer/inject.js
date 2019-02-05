66e189f86fab0b1a88f590574ae90f1a8df3bc9f
Sunil Kumar Vadlamani <sunilteja93@users.noreply.github.com>
2019-02-04 23:11:47 -0800

    Update inject.js

--- Function-Tracer/inject.js
+++ Function-Tracer/inject.js
@@ -1,15 +1,11 @@
 // Code to inject tracecalls.js code in the webpage 
 
-
 //Creates the script tag
 var s = document.createElement('script');
-
 //Gets the tracecalls.js
 s.src = chrome.extension.getURL('tracecalls.js');
-
 s.onload = function() 
 {
     this.parentNode.removeChild(this);
 };
-
 (document.head || document.documentElement).appendChild(s);
